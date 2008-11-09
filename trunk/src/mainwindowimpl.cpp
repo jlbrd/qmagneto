76fb8b5258508ce74cd328b24ba0ca16828f2d86
jlbiord <jlbiord@11ade833-1d4c-0410-8060-571c2f2e4f7d>
2008-11-09 13:24:18 +0000

    - Mise à jour numéro de version

--- trunk/src/mainwindowimpl.cpp
+++ trunk/src/mainwindowimpl.cpp
@@ -23,7 +23,7 @@
 #endif
 #include <QDebug>
 #define QD qDebug() << __FILE__ << __LINE__ << ":"
-#define VERSION "0.3-4"
+#define VERSION "0.3-5"
 //
 MainWindowImpl::MainWindowImpl( QWidget * parent, Qt::WFlags f)
         : QMainWindow(parent, f)
