0e2dfcd1411f257004cc4200f057eca2c93bf659
jlbiord <jlbiord@11ade833-1d4c-0410-8060-571c2f2e4f7d>
2008-12-10 10:04:30 +0000

    -  Change the plugins path (add the installation directory)

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QDir>
 #include "mainwindowimpl.h"
 //
 int main(int argc, char ** argv)
@@ -9,6 +10,12 @@ int main(int argc, char ** argv)
   	font.setPointSize( font.pointSize()+6 );
   	app.setFont( font );
 #endif
+        // change the plugins path (add the installation directory)
+        QStringList list_path ;
+        QDir dir = QDir(qApp->applicationDirPath()+"/QtPlugins/");
+        list_path << dir.absolutePath () << app.libraryPaths ();
+        app.setLibraryPaths( list_path  );
+  
 	MainWindowImpl win;
 	app.connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
 	win.init();
