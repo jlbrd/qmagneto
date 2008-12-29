5cc03af4216973e37c81ab4bed2369135a59fe82
jlbiord <jlbiord@11ade833-1d4c-0410-8060-571c2f2e4f7d>
2008-12-29 10:38:42 +0000

    - Mise a jour version win32 (recup des images)

--- src/recupimages.cpp
+++ src/recupimages.cpp
@@ -8,63 +8,67 @@
 #include <QDebug>
 #define QD qDebug() << __FILE__ << __LINE__ << ":"
 //
-RecupImages::RecupImages( QStringList list, QSqlQuery query ) 
-	: QObject(), m_liste(list), m_query(query)
+RecupImages::RecupImages( QStringList list, QSqlQuery query )
+        : QObject(), m_liste(list), m_query(query)
 {
-	m_http = 0;
+    m_http = 0;
+    QD;
 }
 //
 RecupImages::~RecupImages()
-{
-}
+{}
 void RecupImages::slotRequestFinished(bool err)
 {
-  if( err )
-  {
-    QD << m_http->errorString();
-		return;
-  }
-	if( !m_liste.count() )
-		return;
-	QByteArray data;
-	data = m_http->readAll();
-	
-	QVariant clob(data);
-	m_query.prepare("update images set ok='1', data=:data where icon='" +m_liste.first().replace("'", "$")+ "'");
-	m_query.bindValue(":data", clob);
-	bool rc = m_query.exec();
-	QD << "recuperation de"<<	m_liste.first() << "terminee";
+    if ( err )
+    {
+        QD << m_http->errorString();
+        return;
+    }
+    if ( !m_liste.count() )
+        return;
+    QByteArray data;
+    data = m_http->readAll();
+    if ( data.isEmpty() )
+        return;
+    QVariant clob(data);
+    m_query.prepare("update images set ok='1', data=:data where icon='" +m_liste.first().replace("'", "$")+ "'");
+    m_query.bindValue(":data", clob);
+    bool rc = m_query.exec();
+    QD << "recuperation ok pour:" << m_liste.first() << "taille:" << data.size();
     if (rc == false)
     {
         qDebug() << "Failed to insert record to db" << m_query.lastError();
     }
-	m_http->close();
-	if( m_liste.count() )
-	{
-		m_liste.pop_front();
-		recup();
-	}
+    m_http->close();
+    if ( m_liste.count() )
+    {
+        m_liste.pop_front();
+        recup();
+    }
 }
 
 void RecupImages::recup()
 {
-	if( m_liste.count() == 0 )
-		return;
-	QString icon = m_liste.first();
-	m_http->setHost(icon.section("/", 2, 2));
-  #ifdef Q_OS_WIN32
-	  m_http->get(QUrl::toPercentEncoding("/"+icon.section("/", 3)) );
-  #else
-	m_http->get("/"+icon.section("/", 3) );
-  #endif
-	QD << "get" << m_liste.count() << icon.section("/", 2, 2)<< icon;
+    if ( m_liste.count() == 0 )
+        return;
+    QString icon = m_liste.first();
+#ifdef Q_OS_WIN32
+    QUrl url(icon);
+    m_http->setHost(url.host());
+    m_http->get( url.toString());
+    //QD << "get" << url;
+#else
+    m_http->setHost(icon.section("/", 2, 2));
+    m_http->get("/"+icon.section("/", 3) );
+    //QD << "get" << m_liste.count() << icon.section("/", 2, 2)<< icon;
+#endif
 }
 
 
 void RecupImages::imageToTmp(QString icon, QSqlQuery query, bool isChaine)
 {
-	m_query = query;
-	QString queryString = "select * from images where icon = '" + icon.replace("'", "$")+ "'";
+    m_query = query;
+    QString queryString = "select * from images where icon = '" + icon.replace("'", "$")+ "'";
     bool rc = m_query.exec(queryString);
     if (rc == false)
     {
@@ -72,42 +76,42 @@ void RecupImages::imageToTmp(QString icon, QSqlQuery query, bool isChaine)
         qDebug() << queryString;
         return;
     }
-    if( m_query.next() )
-	{
-		QFile file;
-		if( isChaine )		
-			file.setFileName(QDir::tempPath()+"/qmagnetochaine.jpg");
-		else
-			file.setFileName(QDir::tempPath()+"/qmagnetoprog.jpg");
-		if (!file.open(QIODevice::WriteOnly ))
-		    return;
-		file.write(m_query.value(2).toByteArray());
-		file.close();
-	}
+    if ( m_query.next() )
+    {
+        QFile file;
+        if ( isChaine )
+            file.setFileName(QDir::tempPath()+"/qmagnetochaine.jpg");
+        else
+            file.setFileName(QDir::tempPath()+"/qmagnetoprog.jpg");
+        if (!file.open(QIODevice::WriteOnly ))
+            return;
+        file.write(m_query.value(2).toByteArray());
+        file.close();
+    }
 }
 
 
 void RecupImages::setListe(QStringList liste, QSqlQuery query)
 {
-	if( m_http )
-	{
-		m_http->clearPendingRequests();
-		m_http->abort();
-		m_http->close();
-		delete m_http;
-	}
-	m_http = new QHttp( this );
-	m_query = query;
-	m_liste = liste;
-	connect(m_http, SIGNAL(done(bool)), this, SLOT(slotRequestFinished(bool)) );
-	recup();
+    if ( m_http )
+    {
+        m_http->clearPendingRequests();
+        m_http->abort();
+        m_http->close();
+        delete m_http;
+    }
+    m_http = new QHttp( this );
+    m_query = query;
+    m_liste = liste;
+    connect(m_http, SIGNAL(done(bool)), this, SLOT(slotRequestFinished(bool)) );
+    recup();
 }
 
 
 QPixmap RecupImages::pixmap(QString icon, QSqlQuery query)
 {
-	m_query = query;
-	QString queryString = "select * from images where icon = '" + icon.replace("'", "$")+ "'";
+    m_query = query;
+    QString queryString = "select * from images where icon = '" + icon.replace("'", "$")+ "'";
     bool rc = m_query.exec(queryString);
     if (rc == false)
     {
@@ -115,11 +119,11 @@ QPixmap RecupImages::pixmap(QString icon, QSqlQuery query)
         qDebug() << queryString;
         return false;
     }
-    
-    if( m_query.next() )
-	{
-		return QPixmap::fromImage( QImage::fromData( ( m_query.value(2).toByteArray() ) ) );
-	}
-	//QD << icon << "non trouvé";
-	return QPixmap();
+
+    if ( m_query.next() )
+    {
+        return QPixmap::fromImage( QImage::fromData( ( m_query.value(2).toByteArray() ) ) );
+    }
+    //QD << icon << "non trouvé";
+    return QPixmap();
 }
